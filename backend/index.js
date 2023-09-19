dabe96b7e5f6a2c04c47604b0ab25a7d862d109e
Rashi Gupta <guptarashi578@gmail.com>
2023-09-19 15:23:26 +0530

    server

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+const express = require('express')
+
+const app = express();
+const port = process.env.PORT || 3000
+
+app.listen(port, () => {
+    console.log(`connection is live at port no. ${port}`)
+})
